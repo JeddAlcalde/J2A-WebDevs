92a6b636ba692006ef82721473e3a0e84c1a0753
JeddAlcalde <jeddalcalde@gmail.com>
2022-09-09 19:47:18 -0400

    Add Text Change On Hover
    Project section now has text changing when
    hovering over the corresponding project and
    will give a description of that project.
    Before adding all of this to the other planned
    transitions, I want to learn fade-ins so that I
    can easily copy this to all the other parts of
    the website.

--- about-Jedd/script.js
+++ about-Jedd/script.js
@@ -1,4 +1,5 @@
 function projectText(hovered, item){
+    this.removeAllChildren(document.getElementById("text-changer"));
     if(hovered == true){
         var newh1 = document.createElement("h1");
         newh1.style.fontSize = "325%";
@@ -10,26 +11,37 @@ function projectText(hovered, item){
             case "top":
                 newh1.innerHTML = "The Odin Project";
                 newh4.innerHTML = "The Odin Project is an online open source project used to educate up and coming web developers. I completed the \"Foundations\" part of it, focused on teaching fundamentals of HTML, CSS and JavaScript through articles, exercies and projects. As well as some information on backend. The projects listed for The Odin Project were made to teach specific skills and are therfore not polished as websites.";
+                break;
             case "eas":
                 newh1.innerHTML = "The Odin Project : Etch a Sketch";
                 newh4.innerHTML = "project description for etch a sketch";
+                break;
             case "rps":
                 newh1.innerHTML = "The Odin Project : Rock Paper Scissors";
                 newh4.innerHTML = "project description for rock paper scissors";
+                break;
             case "lp":
                 newh1.innerHTML = "The Odin Project : Landing Page";
                 newh4.innerHTML = "project description for landing page";
+                break;
             case "rec":
                 newh1.innerHTML = "The Odin Project : Recipes";
                 newh4.innerHTML = "project description for recipes";
+                break;
             case "calc":
                 newh1.innerHTML = "The Odin Project : Calculator";
                 newh4.innerHTML = "project description for calculator";
-            default:
-                newh1.innerHTML = "Here are the projects I've done so far!";
-                newh4.innerHTML = "Upon clicking each one you will be offered page links and explanations to each one";
+                break;
+            case "nhnl":
+                newh1.innerHTML = "New Hair New Life";
+                newh4.innerHTML = "project description for new hair new life";
+                break;
+            case "ps":
+                newh1.innerHTML = "My Own Website";
+                newh4.innerHTML = "project description for personal site";
+                break;
         }
-        item.style.background = red;
+        //item.style.background = red;
         document.getElementById("text-changer").appendChild(newh1);
         document.getElementById("text-changer").appendChild(newh4);
     }
@@ -38,9 +50,15 @@ function projectText(hovered, item){
         newh1.innerHTML = "Here are the projects I've done so far!";
         newh1.style.fontSize = "325%";
         var newh4 = document.createElement("h4");
-        newh4.innerHTML = "Upon clicking each one you will be offered page links and explanations to each one";
+        newh4.innerHTML = "Upon hovering each one you will be offered page links and explanations to each one. Hovering will give you a breif description on each.";
         newh4.style.fontSize = "175%";
         document.getElementById("text-changer").appendChild(newh1);
         document.getElementById("text-changer").appendChild(newh4);
+    }
+}
+
+function removeAllChildren(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
     }
 }
